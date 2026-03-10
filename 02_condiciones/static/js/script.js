d77f1cac669545863c6e9999c6f5d91c54ad97d5
Yuli :] <yuliethgonzalez@liceovvh.cl>
2026-03-10 12:45:19 -0300

    .

--- 02_condiciones/static/js/script.js
+++ 02_condiciones/static/js/script.js
@@ -50,3 +50,77 @@ function nuevoProducto() {
         
     }
 }
+
+
+/* 4. Control de Stock Máximo
+Imagina que una bodega solo tiene espacio para 5 cajas.
+let bodega = ["caja1", "caja2", "caja3", "caja4", "caja5", "caja6"];
+Si el tamaño de la bodega (.length) es mayor a 5, 
+elimina la última caja con .pop() y muestra: "Bodega llena, eliminando último ingreso".
+Si es 5 o menos, muestra: "Espacio disponible".
+*/
+
+
+
+
+
+/*
+5. Limpieza de Datos
+A veces recibimos datos basura. let colaEspera = ["error_404", "Juan", "Sofía"];
+Si el primer elemento (índice 0) es igual a "error_404", 
+elimínalo usando .shift() y muestra la cola limpia.
+Si no, muestra: "La lista de espera está correcta".
+*/
+
+
+
+/*6. Acceso VIP
+Tienes una lista de usuarios comunes: let foro = ["User1", "User2"];
+Declara la variable rol.
+Si el rol es "Admin", agrega el nombre al inicio de la lista con .unshift() para que tenga prioridad.
+Si no es Admin, agrégalo al final con .push().
+*/
+
+
+
+/*Bloque 4: Lógica Avanzada y Matrices (Dificultad Alta)
+7. Sistema de Calificaciones Chile (Escala 1 a 7)
+Crea una variable nota.
+Si la nota es 6 o superior: "Excelente, eximido".
+Si la nota es entre 4 y 5.9: "Aprobado, vas a examen".
+Si es menor a 4: "Reprobado".
+(Pista: Usa else if).
+*/
+
+
+
+/*8. Buscador de Invitados
+let invitados = ["Ana", "Luis", "Camila", "Diego"];
+Crea una variable persona.
+Si la persona está en la lista, muestra: "¡Bienvenido/a [nombre]! Pasa a la fiesta".
+Si no está, muestra: "Lo siento, no estás en la lista de invitados".
+*/
+
+
+
+
+
+/*9. El Almacén de Matrices (2D Arrays)
+Imagina una estantería con dos niveles (una matriz):
+let estanteria = [ ["Manzanas", "Peras"], ["Leche", "Yogur"] ];
+Crea una variable seccion (0 o 1) y una variable producto.
+Accede a la sección elegida. Si el producto solicitado 
+está en esa sub-lista, muestra: "Producto encontrado en el estante".
+Si no, muestra: "No tenemos ese producto en esa sección".
+*/
+
+
+
+/* 10. Registro Maestro de Visitas
+Crea un arreglo vacío llamado bitacora.
+Declara las variables nombreVisita y esVip (booleano).
+Si el nombre está vacío: Mostrar error.
+Si el nombre existe y esVip es true: Agregarlo al inicio con .unshift().
+Si el nombre existe y esVip es false: Agregarlo al final con .push().
+Al final, muestra cuántas personas hay en total usando .length.
+*/
