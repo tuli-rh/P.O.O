880f1099c6605ded5f18e1df3eccc6c3dc9f62db
Yuli :] <yuliethgonzalez@liceovvh.cl>
2026-03-12 15:06:07 -0300

    .

--- 03_bucles_for/static/js/script.js
+++ 03_bucles_for/static/js/script.js
@@ -43,21 +43,20 @@ Recorre el arreglo con un for. Si la nota es 4.0 o superior, suma 1 al contador.
 Al terminar el ciclo, muestra en el párrafo pantalla: "Total de alumnos aprobados: [número]".
 */
 
-function buscarAprobados() {
+function aprobados() {
     let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
     let contadorAprobados = 0;
-    const container = document.getElementById('resultado-container3');
-    const result = document.getElementById('result3');
-    result.textContent = "";
-    for (let i = 0; i < notas.length; i++) {
+    const container = document.getElementById('resultado-containertres');
+    const result3 = document.getElementById('result3');
+    for (let i = 0; i <= notas.length; i++) {
         if (notas[i] >= 4.0) {
-            contadorAprobados++ 
+            contadorAprobados++;
         }
     }
-   
-    result = `Total de alumnos aprobados: ${contadorAprobados}.`
+    result3.textContent = "Total de alumnos aprobados: " + contadorAprobados;
     container.classList.remove('d-none');
 }
+
 
 /* 4. El Filtro de Inventario
 Una tienda quiere mostrar solo los productos que están disponibles.
@@ -67,7 +66,17 @@ Si el producto es diferente de "Agotado" (!==), agrégalo al texto del párrafo 
 Si dice "Agotado", sáltalo.
 */
 function filtroInventario() {
-    
+    let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+    const container = document.getElementById('resultado-container4');
+    const result4 = document.getElementById('result4');
+    let disponible = [];
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i] !== "Agotado") {
+            disponible.push(productos[1])
+        }
+    }
+    result4.textContent = `productos disponibles: ${disponible}`;
+    container.classList.remove('d-none');
 }
 
 
@@ -78,7 +87,19 @@ Crea una variable totalRecaudado que inicie en 0.
 Usa un ciclo for para sumar cada uno de los aportes a la variable total.
 Al finalizar el ciclo, escribe en el párrafo: "La colecta reunió un total de: $[total]".
 */
+function operarIntruso() {
+    let aportes = [1500, 2000, 500, 3000, 1000];
+    let totalRecaudado = 0;
+    const container = document.getElementById('resultado-container5');
+    const result5 = document.getElementById('result5');
 
+    for (let i = 0; i < aportes.length; i++) {
+        totalRecaudado += aportes[1];
+
+    }
+    result5.textContent = `La colecta reunio un total de: ${totalRecaudado}`;
+    container.classList.remove('d-none');
+}
 
 /* 6. Formateador de Nombres VIP
 Tienes una lista de usuarios: let asistentes = ["carlos", "MARIA", "pedro", "LUCIA"];
@@ -86,7 +107,24 @@ Queremos mostrarlos en el HTML, pero destacando a algunos.
 Recorre el arreglo. Si el índice actual (i) es par (puedes usar i % 2 === 0), 
 agrega la palabra " [VIP]" al lado de su nombre y muéstralo en el párrafo. Si es impar, muéstralo normal.
 */
+function nombreVIP() {
+    let asistentes = ["carlos", "MARIA", "pedro", "LUCIA"];
+    let resultado = [];
+    const container = document.getElementById('resultado-container6');
+    const result6 = document.getElementById('result6');
 
+    for (let i = 0; i < asistentes.length; i++) {
+        if (i % 2 === 0) {
+            resultado.push(`${asistentes[i]} [VIP]`);
+        } else {
+            resultado.push(`${asistentes[i]} `);
+        }
+
+    }
+    result6.textContent = resultado.join(" - ");
+    container.classList.remove('d-none');
+
+}
 
 /* 7. El Buscador de Stock Específico
 Tienes un inventario: let bodega = ["Lápiz", "Cuaderno", "Goma", "Cuaderno", "Regla", "Cuaderno"];
@@ -95,6 +133,21 @@ Recorre la bodega con un for. Cada vez que el elemento de la lista sea igual al 
 aumenta tu contador.
 Al final, inyecta en el párrafo: "El artículo [articuloBuscado] se encuentra [veces] veces en la bodega".
 */
+function buscadorStock() {
+    let bodega = ["Lápiz", "Cuaderno", "Goma", "Cuaderno", "Regla", "Cuaderno"];
+    let articuloBuscado = prompt("Ingrese el articulo que busca: ");
+    let vecesEncontrado = 0;
+    const container = document.getElementById('resultado-container7');
+    const result7 = document.getElementById('result7');
+
+    for (let i = 0; i < bodega.length; i++) {
+        if (articuloBuscado == bodega[i]) {
+            vecesEncontrado++
+        }
+    }
+    result7.textContent = `El artículo ${articuloBuscado} se encuentra ${vecesEncontrado} veces en la bodega`;
+    container.classList.remove('d-none');
+}
 
 
 /* 8. Generador de Párrafos de Advertencia
@@ -106,3 +159,16 @@ Recorre el arreglo. Si la temperatura es mayor a 30,
 inyecta en el párrafo: "¡ALERTA! Temperatura crítica de " + temperatura + " grados.<br>".
 (Nota: el <br> hará que cada alerta aparezca en una nueva línea en la pantalla).
 */
+
+function advertencia() {
+    let temperaturas = [22, 24, 28, 35, 21, 38];
+    const container = document.getElementById('resultado-containerocho');
+    const result8 = document.getElementById('result8');
+    result8.innerHTML = "";
+    for (let i = 0; i < temperaturas.length; i++) {
+        if (temperaturas[i] > 30) {
+            result8.innerHTML += "¡ALERTA! Temperatura crítica de " + temperaturas[i] + " grados.<br>";
+        }
+    }
+    container.classList.remove('d-none');
+}
